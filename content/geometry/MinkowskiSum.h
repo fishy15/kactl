46613f0b2792032121802ae2ff00e53e18019b51
= <38575855+fishy15@users.noreply.github.com>
2024-05-21 13:01:34 -0500

    Add Minkowski sum template

---
+++ content/geometry/MinkowskiSum.h
@@ -0,0 +1,42 @@
+/**
+ * Author: SYury, fishy15
+ * Date: 2024-05-21
+ * License: CC BY-SA
+ * Source: https://cp-algorithms.com/geometry/minkowski.html
+ * Description: Returns the set of all sums of points of two
+ * convex polygons.
+ * Status: tested on https://contest.ucup.ac/contest/1382/problem/7569
+ * Time: O(n + m)
+ */
+#pragma once
+#include "Point.h"
+
+typedef Point<ll> P;
+void reorder_polygon(vector<P> &p) {
+    int pos = 0;
+    for (int i = 1; i < sz(p); i++) {
+        if (p[i].y < p[pos].y || (p[i].y == p[pos].y && p[i].x < p[pos].x))
+            pos = i;
+    }
+    rotate(p.begin(), p.begin() + pos, p.end());
+}
+
+vector<P> minkowski(vector<P> p, vector<P> q) {
+    reorder_polygon(p);
+    reorder_polygon(q);
+
+    p.push_back(p[0]);
+    p.push_back(p[1]);
+    q.push_back(q[0]);
+    q.push_back(q[1]);
+
+    vector<P> result;
+    int i = 0, j = 0;
+    while (i < sz(p) - 2 || j < sz(q) - 2) {
+        result.push_back(p[i] + q[j]);
+        auto cross = (p[i + 1] - p[i]).cross(q[j + 1] - q[j]);
+        if (cross >= 0 && i < sz(p) - 2) ++i;
+        if (cross <= 0 && j < sz(q) - 2) ++j;
+    }
+    return result;
+}
