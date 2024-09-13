ad8628c274c5074c58500e9d7cb3af2a32adcf73
Aaryan Prakash <aprakash@cs.utexas.edu>
2024-09-12 20:15:08 -0500

    Update BCC template

--- content/graph/BiconnectedComponents.h
+++ content/graph/BiconnectedComponents.h
@@ -19,35 +19,31 @@
  */
 #pragma once
 
-vi num, st;
-vector<vector<pii>> ed;
-int Time;
 template<class F>
-int dfs(int at, int par, F& f) {
-	int me = num[at] = ++Time, top = me;
-	for (auto [y, e] : ed[at]) if (e != par) {
-		if (num[y]) {
-			top = min(top, num[y]);
-			if (num[y] < me)
-				st.push_back(e);
-		} else {
-			int si = sz(st);
-			int up = dfs(y, e, f);
-			top = min(top, up);
-			if (up == me) {
-				st.push_back(e);
-				f(vi(st.begin() + si, st.end()));
-				st.resize(si);
+void bicomps(vector<vector<pii>> &ed, F f) {
+	vi num(sz(ed)), st;
+	int t=0;
+	auto dfs = [&](auto &&self, int at, int par) -> int {
+		int me = num[at] = ++t, top = me;
+		for (auto [y, e] : ed[at]) if (e != par) {
+			if (num[y]) {
+				top = min(top, num[y]);
+				if (num[y] < me)
+					st.push_back(e);
+			} else {
+				int si = sz(st);
+				int up = self(self, y, e);
+				top = min(top, up);
+				if (up == me) {
+					st.push_back(e);
+					f(vi(st.begin() + si, st.end()));
+					st.resize(si);
+				}
+				else if (up < me) st.push_back(e);
+				else { /* e is a bridge */ }
 			}
-			else if (up < me) st.push_back(e);
-			else { /* e is a bridge */ }
 		}
-	}
-	return top;
-}
-
-template<class F>
-void bicomps(F f) {
-	num.assign(sz(ed), 0);
-	rep(i,0,sz(ed)) if (!num[i]) dfs(i, -1, f);
+		return top;
+	};
+	rep(i,0,sz(ed)) if (!num[i]) dfs(dfs, i, -1);
 }
