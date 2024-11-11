7231908200d236b5d116a10f43c5967cd048bd28
Aaryan Prakash <aprakash@cs.utexas.edu>
2024-11-11 14:11:20 -0600

    Update compile script

--- doc/scripts/test-compiles.sh
+++ doc/scripts/test-compiles.sh
@@ -4,4 +4,4 @@ echo "
 #include \"../content/contest/template.cpp\"
 #include \"../$FILE\"
 " >build/temp.cpp
-g++ -Wall -Wextra -Wfatal-errors -Wconversion -std=c++17 build/temp.cpp && rm a.out build/temp.cpp
+g++ build/temp.cpp -O2 -std=c++17 -Wall -Wextra -Wfatal-errors -Wshadow -Wconversion && rm a.out build/temp.cpp
