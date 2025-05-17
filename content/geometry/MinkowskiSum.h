11d2c8c67673cf87f0ffe68f0dbafbee471501e0
= <38575855+fishy15@users.noreply.github.com>
2025-05-17 15:35:00 -0500

    Use Kuroni's Minkowski Sum implementation

--- content/geometry/MinkowskiSum.h
+++ content/geometry/MinkowskiSum.h
@@ -1,42 +1,45 @@
 /**
- * Author: SYury, fishy15
- * Date: 2024-05-21
- * License: CC BY-SA
- * Source: https://cp-algorithms.com/geometry/minkowski.html
- * Description: Returns the set of all sums of points of two
- * convex polygons.
- * Status: tested on https://contest.ucup.ac/contest/1382/problem/7569
- * Time: O(n + m)
+ * Author: Kuroni
+ * Date: 2023-10-14
+ * Source: me
+ * Description: Minkowski sum of set of convex ccw polygons.
+ * Time: $O(P \log N)$, where $P$ is number of points and $N$ is number of polygons.
+ * Status: Tested on wf21:K, cf:87E.
  */
 #pragma once
+
 #include "Point.h"
 
 typedef Point<ll> P;
-void reorder_polygon(vector<P> &p) {
-	int pos = 0;
-	for (int i = 1; i < sz(p); i++) {
-		if (p[i].y < p[pos].y || (p[i].y == p[pos].y && p[i].x < p[pos].x))
-			pos = i;
+vector<P> minkowskiSum(vector<vector<P>> hs) {
+	auto cmp = [](P a, P b) {
+		return make_pair(a.x < 0 || a.x == 0 && a.y < 0, a.y * (ll)b.x)
+			< make_pair(b.x < 0 || b.x == 0 && b.y < 0, a.x * (ll)b.y);
+	};
+	typedef tuple<P, int, int> T;
+	auto cmp_tup = [&cmp](T a, T b) {
+		auto& [pa, ja, ia] = a;
+		auto& [pb, jb, ib] = b;
+		if (cmp(pa, pb)) return false;
+		if (cmp(pb, pa)) return true;
+		return make_pair(ja, ia) < make_pair(jb, ib);
+	};
+	priority_queue<T, vector<T>, decltype(cmp_tup)> pq(cmp_tup);
+	P cur = P();
+	int s = 0, t = 0;
+	rep(i, 0, sz(hs)) {
+		auto& v = hs[i];
+		rotate(begin(v), min_element(all(v)), end(v));
+		if (sz(v) > 1) pq.push({v[1] - v[0], 0, i}), s += sz(v);
+		cur = cur + v[0];
 	}
-	rotate(p.begin(), p.begin() + pos, p.end());
-}
-
-vector<P> minkowski(vector<P> p, vector<P> q) {
-	reorder_polygon(p);
-	reorder_polygon(q);
-
-	p.pb(p[0]);
-	p.pb(p[1]);
-	q.pb(q[0]);
-	q.pb(q[1]);
-
-	vector<P> result;
-	int i = 0, j = 0;
-	while (i < sz(p) - 2 || j < sz(q) - 2) {
-		result.pb(p[i] + q[j]);
-		auto cross = (p[i + 1] - p[i]).cross(q[j + 1] - q[j]);
-		if (cross >= 0 && i < sz(p) - 2) ++i;
-		if (cross <= 0 && j < sz(q) - 2) ++j;
+	vector<P> h(s + 1);
+	for (h[t++] = cur; sz(pq);) {
+		auto [p, j, i] = pq.top(); pq.pop();
+		t -= (t >= 2 && !cmp(h[t - 1] - h[t - 2], p));
+		h[t++] = (cur = cur + p);
+		auto& v = hs[i];
+		if (++j < sz(v)) pq.push({v[(j + 1) % sz(v)] - v[j], j, i});
 	}
-	return result;
+	return {h.begin(), h.begin() + t - (t >= 2 && h[0] == h[t - 1])};
 }
