ac5fed04eb75861a6885f4c7fedcc8a612468086
= <38575855+fishy15@users.noreply.github.com>
2025-05-17 15:33:37 -0500

    Move Dylan's string templates

--- content/strings/AhoCorasick.h
+++ content/strings/AhoCorasick.h
@@ -1,85 +1,45 @@
 /**
- * Author: Simon Lindholm
- * Date: 2015-02-18
+ * Author: Dylan Smith
+ * Date: 2024-11-13
  * License: CC0
- * Source: marian's (TC) code
- * Description: Aho-Corasick automaton, used for multiple pattern matching.
- * Initialize with AhoCorasick ac(patterns); the automaton start node will be at index 0.
- * find(word) returns for each position the index of the longest word that ends there, or -1 if none.
- * findAll($-$, word) finds all words (up to $N \sqrt N$ many if no duplicate patterns)
- * that start at each position (shortest first).
- * Duplicate patterns are allowed; empty patterns are not.
- * To find the longest words that start at each position, reverse all input.
- * For large alphabets, split each symbol into chunks, with sentinel bits for symbol boundaries.
- * Time: construction takes $O(26N)$, where $N =$ sum of length of patterns.
- * find(x) is $O(N)$, where N = length of x. findAll is $O(NM)$.
- * Status: stress-tested
+ * Source: idk
+ * Description: Constructs Aho-Corasick automaton for given list of words
+ * Time: Construction is $O(N)$, move() is amortized $O(N)$.
+ * Status: Used in Yandex Semifinal 2024 Problem F
  */
 #pragma once
 
 struct AhoCorasick {
-	enum {alpha = 26, first = 'A'}; // change this!
 	struct Node {
-		// (nmatches is optional)
-		int back, next[alpha], start = -1, end = -1, nmatches = 0;
-		Node(int v) { memset(next, v, sizeof(next)); }
+		map<char, int> nxt;
+		int lnk = 0, cnt = 0;
 	};
-	vector<Node> N;
-	vi backp;
-	void insert(string& s, int j) {
-		assert(!s.empty());
-		int n = 0;
-		for (char c : s) {
-			int& m = N[n].next[c - first];
-			if (m == -1) { n = m = sz(N); N.emplace_back(-1); }
-			else n = m;
-		}
-		if (N[n].end == -1) N[n].start = j;
-		backp.pb(N[n].end);
-		N[n].end = j;
-		N[n].nmatches++;
-	}
-	AhoCorasick(vector<string>& pat) : N(1, -1) {
-		rep(i,0,sz(pat)) insert(pat[i], i);
-		N[0].back = sz(N);
-		N.emplace_back(0);
-
-		queue<int> q;
-		for (q.push(0); !q.empty(); q.pop()) {
-			int n = q.front(), prev = N[n].back;
-			rep(i,0,alpha) {
-				int &ed = N[n].next[i], y = N[prev].next[i];
-				if (ed == -1) ed = y;
-				else {
-					N[ed].back = y;
-					(N[ed].end == -1 ? N[ed].end : backp[N[ed].start])
-						= N[y].end;
-					N[ed].nmatches += N[y].nmatches;
-					q.push(ed);
+	vector<Node> t;
+	AhoCorasick(vector<string> words) {
+		t.pb({});
+		for (string s : words) {
+			int cur = 0;
+			for (char c : s) {
+				if (!t[cur].nxt[c]) {
+					t[cur].nxt[c] = sz(t);
+					t.pb({});
 				}
+				cur = t[cur].nxt[c];
+			}
+			t[cur].cnt++;
+		}
+		queue<int> q; q.push(0);
+		while (!q.empty()) {
+			int u = q.front(); q.pop();
+			assert(0 <= u && u < sz(t));
+			for (auto &p : t[u].nxt) {
+				if (u) t[p.second].lnk = move(t[u].lnk, p.first);
+				q.push(p.second);
 			}
 		}
 	}
-	vi find(string word) {
-		int n = 0;
-		vi res; // ll count = 0;
-		for (char c : word) {
-			n = N[n].next[c - first];
-			res.pb(N[n].end);
-			// count += N[n].nmatches;
-		}
-		return res;
-	}
-	vector<vi> findAll(vector<string>& pat, string word) {
-		vi r = find(word);
-		vector<vi> res(sz(word));
-		rep(i,0,sz(word)) {
-			int ind = r[i];
-			while (ind != -1) {
-				res[i - sz(pat[ind]) + 1].pb(ind);
-				ind = backp[ind];
-			}
-		}
-		return res;
+	int move(int u, int c) {
+		if (t[u].nxt[c]) return t[u].nxt[c];
+		return u == 0 ? 0 : t[u].nxt[c] = move(t[u].lnk, c);
 	}
 };
