e9c7a9c234dc762044a65895e7282df86cef92f1
= <38575855+fishy15@users.noreply.github.com>
2025-05-17 15:32:07 -0500

    Touch up flow with demands

--- content/graph/FlowWithDemands.h
+++ content/graph/FlowWithDemands.h
@@ -6,8 +6,8 @@
  * everything, and new edges from everything to $t'$. Define:
  * \begin{itemize}
  * \item $c'((s', v)) = \sum_{u \in V} d((u,v))$ for each edge $(s', v)$
- * \item $c'((v, t')) = \sum_{w \in V} d((v,w))$ for each edge $(s, t')$
- * \item $c'((u,v)) = c((u,v)) - d((u,v))$ for each old edge
+ * \item $c'((v, t')) = \sum_{w \in V} d((v,w))$ for each edge $(v, t')$
+ * \item $c'((u,v)) = c((u,v)) - d((u,v))$ for each old edge $(u, v)$
  * \item $c'((t,s)) = \infty$
  * \end{itemize}
  * Status: works
