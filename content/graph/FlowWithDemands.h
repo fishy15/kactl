4cfe9d620a243a63cc3383d96519e7789c28b930
= <38575855+fishy15@users.noreply.github.com>
2025-05-17 15:25:25 -0500

    Add the file

---
+++ content/graph/FlowWithDemands.h
@@ -0,0 +1,14 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2015-05-13
+ * Source: Wikipedia
+ * Description: Add a new source $s'$ and sink $t'$, new edges from $s'$ to 
+ * everything, and new edges from everything to $t'$. Define:
+ * \begin{itemize}
+ * \item $c'((s', v)) = \sum_{u \in V} d((u,v))$ for each edge $(s', v)$
+ * \item $c'((v, t')) = \sum_{w \in V} d((v,w))$ for each edge $(s, t')$
+ * \item $c'((u,v)) = c((u,v)) - d((u,v))$ for each old edge
+ * \item $c'((t,s)) = \infty$
+ * \end{itemize}
+ * Status: works
+ */
